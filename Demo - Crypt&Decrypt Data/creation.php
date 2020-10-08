d061d3c7bb1f42e819d3c34eda6453afe8289235
Deadman <enzo.richard2014@gmail.com>
2020-10-08 19:23:48 +0200

    Update creation.php

--- Demo - Crypt&Decrypt Data/creation.php
+++ Demo - Crypt&Decrypt Data/creation.php
@@ -8,17 +8,18 @@ $key = generate_user_key();
 
 
 // Encrypting data
-$prenom = encrypt("John", $iv, $key);
-$nom = encrypt("Doe", $iv, $key);
-$mail = encrypt("contact@contact.com", $iv, $key);
-$telephone = encrypt("+33648756935", $iv, $key);
-$login = "mySuperLogin";
+$prenom = encrypt(htmlspecialchars(strip_tags("John")), $iv, $key);
+$nom = encrypt(htmlspecialchars(strip_tags("Doe")), $iv, $key);
+$mail = encrypt(htmlspecialchars(strip_tags("contact@contact.com")), $iv, $key);
+$telephone = encrypt(htmlspecialchars(strip_tags("+33648756935")), $iv, $key);
+$login = htmlspecialchars(strip_tags("mySuperLogin"));
 // In this example, password is not encrypted, but you should not use this method to encrypt password but using
 // password_hash() instead, so password won't be decryptable
-$password = "myS3cu4edP@ssw04d";
+$password = htmlspecialchars(strip_tags("myS3cu4edP@ssw04d"));
 
 $query = "INSERT INTO Users(nom, prenom, mail, telephone, login, password) VALUES('$nom', '$prenom', '$mail', '$telephone', '$login', '$password')";
-mysqli_query($db, $query);
+$result = $bddPDO->prepare($query);
+$result->execute();
 
 // Register after Users otherwise constraint key fail
 
@@ -26,5 +27,6 @@ mysqli_query($db, $query);
 // It's stored to decrypt later
 // Here UserID = 1, should remplace with $_SESSION['id']
 $query = "INSERT INTO Crypto(uid, iv, cle) VALUES(1, '$iv', '$key')";
-mysqli_query($db, $query);
+$result = $bddPDO->prepare($query);
+$result->execute();
 ?> 
