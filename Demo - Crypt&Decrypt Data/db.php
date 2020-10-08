079312e3356563c1490c6a71637f117573d56fb0
Deadman <enzo.richard2014@gmail.com>
2020-10-08 19:21:09 +0200

    Update db.php

--- Demo - Crypt&Decrypt Data/db.php
+++ Demo - Crypt&Decrypt Data/db.php
@@ -3,17 +3,10 @@ $host = "localhost";
 $user_mysql = "admin";    // nom de l'utilisateur MySQL 
 $password_mysql = "admin";    // mot de passe de l'utilisateur MySQL
 $database = "crypto";
-$db = mysqli_connect($host, $user_mysql, $password_mysql, $database);
-if(!$db)
-{
-    echo "Echec de la connexion\n";
-    exit();
-}
-mysqli_set_charset($db, "utf8");
 
 try
 {
-    $bddPDO = new PDO('mysql:host=localhost;dbname=crypto;charset=utf8', 'admin', 'admin');
+    $bddPDO = new PDO("mysql:host=".$host.";dbname=".$database.";charset=utf8", $user_mysql, $password_mysql);
 }
 catch(Exception $e)
 {
