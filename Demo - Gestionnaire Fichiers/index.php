6342f0f727ed18d81fd5d054a5de88dc998ea27f
Deadman <enzo.richard2014@gmail.com>
2021-01-27 00:29:44 +0100

    Update index.php

--- Demo - Gestionnaire Fichiers/index.php
+++ Demo - Gestionnaire Fichiers/index.php
@@ -3,7 +3,7 @@
       $cookieExpiration = 3600; // cookie expiration time (in seconds). Cookies is overused. Should not set it under 60.
       $attempsLockFail = 3; // After 3 wrong attempts, user will be blocked for the time provided below
       $timeLockFail = 60; // if user fail more than X attempts, he will be blocked for the time provided here
-      $password = "admin"; // password to connect
+      $password = "changeme"; // password to connect
 
       // DONT TOUCH BELOW
 
